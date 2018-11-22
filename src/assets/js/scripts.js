a283cb8ff6028ecf41b19cb050859d4e5ccbfdd4
Erik Smith <github@codeblind.io>
2018-11-21 20:10:40 -0500

    Initial commit

---
+++ src/assets/js/scripts.js
@@ -0,0 +1,33 @@
+(function () {
+  function mobileNav() {
+    var nav = document.getElementById('nav-main');
+
+    document.getElementById('nav-toggle__more').addEventListener('click', function () {
+      nav.classList.add('expanded');
+    });
+
+    document.getElementById('nav-toggle__less').addEventListener('click', function () {
+      nav.classList.remove('expanded');
+    });
+
+    function watchResize(el) {
+      function unsetExpanded(mQ) {
+        if (mQ.matches) {
+          el.classList.remove('expanded');
+        }
+      }
+
+      if (matchMedia) {
+        var mQ = window.matchMedia("(min-width: 480px)");
+        mQ.addListener(unsetExpanded);
+        unsetExpanded(mQ);
+      }
+    }
+
+    watchResize(nav);
+  }
+
+  window.addEventListener('load', function () {
+    mobileNav();
+  });
+})();
